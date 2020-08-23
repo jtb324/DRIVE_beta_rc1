e9170f15e270ef1e550a337916578b7cd554afcf
jtb324 <james.baker@vanderbilt.edu>
2020-08-23 12:29:03 -0500

    This is the shell script used to run plink and tho split the excel file of variants to variants per chromosome.

---
+++ shell_script/split_file.sh
@@ -0,0 +1,5 @@
+python3 split_input.py \
+    --input /data100t1/home/james/MEGA_CLI_project/Testrun/clinVar_8_21_20/ClinVar_Target_set01.xlsx \
+    --output /data100t1/home/james/MEGA_CLI_project/Testrun/clinVar_8_21_20/ \
+    --bfile /data100t1/share/BioVU/mega_0519/MEGAex_BioVUthru2019-02_BestOfMultipleCalls/MEGA_ex_Array_BestOfMultipleCalls_Below_Stuttering_A1 \
+    --var_list_dir /data100t1/home/james/MEGA_CLI_project/Testrun/clinVar_8_21_20/variants_of_interest/
