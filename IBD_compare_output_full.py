6a638dc735e4f45d7c018bf500778627be858569
jtb324 <james.baker@vanderbilt.edu>
2020-09-29 20:05:56 -0500

    updated the script so that it only outputs the final file which is the single row containing a list of the maximum number of pairs found. This list is then reformated so that each pair is shown per row. Also fixed a bug where a newline was being added to the last pair.

--- IBD_compare_output_full.py
+++ IBD_compare_output_full.py
@@ -1,7 +1,5 @@
-from pydoc import cli
 import sys
 import gzip
-from numpy.core.numeric import NaN
 import pandas as pd
 import itertools
 import glob
@@ -36,6 +34,8 @@ def build_file_dict(ibd_file_list: list, program_list: list) -> dict:
     # iterate through the files to build the dictionary
     for ibd_file in ibd_file_list:
 
+        print(ibd_file)
+
         match = re.search(r'.chr\d\d\.', ibd_file)
 
         # find chromosome number
@@ -52,8 +52,8 @@ def build_file_dict(ibd_file_list: list, program_list: list) -> dict:
             chr_num = match.group(0)
 
         print(chr_num)
-        # Finding the variant id of the file. file names are built so that the variant id sits
-        # between the first "_" and the first "."
+        # Finding the variant id of the file. file names are built so that the
+        # variant id sits between the first "_" and the first "."
         for ibd_program in program_list:
 
             if ibd_program in ibd_file:
@@ -67,8 +67,10 @@ def build_file_dict(ibd_file_list: list, program_list: list) -> dict:
 
         variant_id = ibd_file[ibd_program_indx:dot_indx]
 
-        # There are a few variants that have a dot in the middle and therefore the above code would split it.
-        # Therefore if there is no number in the string than it enters this if statement and then finds the next dot.
+        # There are a few variants that have a dot in the middle and therefore
+        # the above code would split it.
+        # Therefore if there is no number in the string than it enters this if
+        # statement and then finds the next dot.
         if not any(map(str.isdigit, variant_id)):
             # This finds the second dot in the string
             dot_indx = ibd_file.find(".", dot_indx+1)
@@ -76,7 +78,8 @@ def build_file_dict(ibd_file_list: list, program_list: list) -> dict:
             # This finds the full variant id
             variant_id = ibd_file[ibd_program_indx:dot_indx]
 
-        # using list comprehension to get all the files that contain that variant and chromosome
+        # using list comprehension to get all the files that contain that
+        # variant and chromosome
         filter_ibd_file_list = [
             file for file in ibd_file_list if variant_id in file and chr_num in file]
 
@@ -86,7 +89,8 @@ def build_file_dict(ibd_file_list: list, program_list: list) -> dict:
             # This goes through the three files in the filter_ibd_file_list
             for file in filter_ibd_file_list:
 
-                # This checks to see if the variant id is in the dictionary and that the ibd program is in the file
+                # This checks to see if the variant id is in the dictionary 
+                # and that the ibd program is in the file
                 if ibd_program in file and (chr_num, variant_id) not in file_dict.keys():
 
                     # If it is not then the
@@ -143,47 +147,18 @@ def all_agree_pair(pair_list: dict) -> list:
     return unionpair
 
 
-def get_max_pairs(allpair_file_path: str, output_path: str, variant_id: str, carrier_file_dir: str, chr_num: str):
+def get_max_pairs(allpair_file_name: str, pairs_row: str, variant_id: str, carrier_file_dir: str, chr_num: str):
     '''This function will find the highest number of pairs and writes it to a file called .allpair.txt'''
-    # Opening the allpair file
-    print(allpair_file_path)
-    with open(allpair_file_path, "r") as allpair_file:
-        max_pairs = 0
-        count = 0
-        # Going through each row
-        for row in allpair_file:
 
-            pair_count = row.split("\t")[3]
+    # spliting the pair row
+    # {str(CHR)}\t{str(pos)}\tNA\t{len(newallpair)}\t{' '.join(outpair)}
 
-            try:
+    pair_str:str = pairs_row.split("\t")[4]
 
-                if int(pair_count) < max_pairs:
-                    count += 1
-                    if count == 1:
-                        end_pos = row.split("\t")[1]
+    pair_list:list = pair_str.split(" ")
 
-                elif int(pair_count) > max_pairs:
-                    max_pairs = int(row.split("\t")[3])
-                    previous_row = row
-                    start_pos = previous_row.split("\t")[1]
-
-            except ValueError:
-                pass
-
-        # This creates the file path up to .allpair.txt. It is upto that point because it will be reused for the allpair.new.txt
-        write_path = "".join([output_path, ".", start_pos, "_", end_pos])
-
-        print(write_path)
-
-        with open("".join([write_path, ".allpair.txt"]), "w") as single_allpair_file:
-            single_allpair_file.write('chr\tpos\tsegments\tnpair\tpair.list\n')
-            single_allpair_file.write(previous_row)
-
-        single_allpair_file.close()
-    allpair_file.close()
-    reformat("".join([write_path, ".allpair.txt"]),
-             write_path, variant_id, carrier_file_dir, chr_num)
-    # Need to make the reformat function that takes that and then writes out line by line
+    reformat(allpair_file_name,
+             pair_list, variant_id, carrier_file_dir, chr_num)
 
 
 def get_carrier_file_list(carrier_files_dir: str) -> list:
@@ -212,74 +187,101 @@ def get_carrier_list(file: str, variant_id: str) -> list:
     carrier_list = carrier_df[carrier_df["Variant ID"]
                               == variant_id]["IID"].values.tolist()
 
-    print(carrier_list)
-
     return carrier_list
 
 
-def reformat(single_allpair_file: str, write_path: str, variant_id: str, carrier_file_dir: str, chr_num: str):
+def reformat(write_path: str, pair_list: list, variant_id: str, carrier_file_dir: str, chr_num: str):
     '''this function takes the original allpair.txt file and reformats it to four columns.
     The first columnn is the ibd program that identified the pairs, the second column is the 
     first pair, the third column is the second pair, and then the fourth column tells if the 
     second pair is a carrier'''
-
+    
+    #Removing the allpair.txt file if it already exists from a previous run
+    if os.path.isfile('filename.txt'):
+    
+        #removing the file
+        os.remove(write_path)
+    
     carrier_list = get_carrier_file_list(carrier_file_dir)
 
     # use list comprehension to find the file with that chr_num
 
-    print(carrier_list)
     carrier_file = [
         file for file in carrier_list if chr_num.strip(".") in file][0]
 
     # getting the list of carriers' iids for the specific variant
-    print(variant_id)
 
     carrier_iid_list = get_carrier_list(carrier_file, variant_id)
 
-    with open(single_allpair_file, "r") as allpair_file:
-        next(allpair_file)
-        for row in allpair_file:
-            pair_str = row.split("\t")[4]
-            pair_list = pair_str.split(" ")
-            header = row.split("\t")[0:4]
+    for pair in pair_list:
 
-            with open("".join([write_path, ".allpair.new.txt"]), "w") as allpair_new_file:
+        with open(write_path, "a+") as allpair_new_file:
+            #Checks to see if the file is empty. If it is empty then it write the header
+            if os.path.getsize(write_path) == 0:
 
                 allpair_new_file.write(
                     "IBD_programs\tpair_1\tpair_2\tcarrier_status\n")
 
-                for pair in pair_list:
-                    # getting the IBD programs that found the output
-                    programs = pair.split(":")[0]
+            # getting the IBD programs that found the output
+            programs = pair.split(":")[0]
 
-                    # getting pair 1
-                    pair1 = pair.split(":")[1].split("-")[0]
+            # getting pair 1
+            pair1 = pair.split(":")[1].split("-")[0]
+            print(pair1) 
+            #Removing the extra newline off of the final pair
+            pair1 = pair1.strip("\n")
 
-                    # getting pair 2
-                    pair2 = pair.split(":")[1].split("-")[1]
+            # getting pair 2
+            pair2 = pair.split(":")[1].split("-")[1]
+            
+            #stripping the extra newline off of the final pair
+            pair2 = pair2.strip("\n")
 
-                    # determining carrier status of second iid pair
-                    if pair2 in carrier_iid_list:
-                        # the carrier status is a one if the second iid pair is in the list of carriers
-                        car_status = 1
+            # determining carrier status of second iid pair
+            if pair2 in carrier_iid_list:
+                # the carrier status is a one if the second iid pair is in the list of carriers
+                car_status = 1
 
-                    else:
-                        # If the second iid pair is not in the list of carriers then the status is a 0
-                        car_status = 0
+            else:
+                # If the second iid pair is not in the list of carriers then the status is a 0
+                car_status = 0
 
-                    # writing the pairs to different columns
-                    allpair_new_file.write(
-                        f"{programs}\t{pair1}\t{pair2}\t{car_status}\n")
+            # writing the pairs to different columns
+            allpair_new_file.write(
+                f"{programs}\t{pair1}\t{pair2}\t{car_status}\n")
 
+
+def is_max_pairs_found(curr_max_pairs: int, new_max_pairs: int) -> int:
+
+    pair_handler_dict = {
+        False: 0,
+        True: 1
+    }
+
+    # This function will return either 1 or zero from the pair handler_dict based on whether or not curr_max_pair from the previous row is less than or greater than the max pairs from the current row
+    return pair_handler_dict[curr_max_pairs >= new_max_pairs]
+
+
+def after_max_pair_found(curr_max_pair: int, new_max_pair: int) -> int:
+    '''This function will break out of the loop if a max pair is found and then the size of the pair list starts decreasing'''
+
+    # If the previous rows max number of pairs is higher than the current row then this function will return a one
+    if curr_max_pair > new_max_pair:
+        return 1
+    elif curr_max_pair == new_max_pair:  # If the the above is not true than it returns a 0
+        return 0
 
 def run(args):
-    ibd_file_list = gather_ibd_files(args.segment_dir)
+    ibd_file_list: list = gather_ibd_files(args.segment_dir)
 
-    file_dict = build_file_dict(ibd_file_list, args.ibd_programs)
+    # Setting a max_number of pairs parameter ot use for comparision so that it only keeps one line
+    max_pairs: int = 0
+
+    file_dict: dict = build_file_dict(ibd_file_list, args.ibd_programs)
 
     for chr_num, variant_id in file_dict.keys():
 
-        file_list = file_dict[(chr_num, variant_id)]
+        file_list: list = file_dict[(chr_num, variant_id)]
 
         if len(file_list) == 0:  # Checking length of system arguments
             sys.exit(f"no files found for this variant {variant_id}")
@@ -328,27 +330,33 @@ def run(args):
                 allcomb['+'.join(item)] = item
 
         combtab = pd.DataFrame(0, columns=files.keys(), index=allcomb.keys())
+
         for item in allcomb:
             combtab.loc[item, allcomb[item]] = len(allcomb[item])
         print("printing output path...")
         print(out)
         # sumtab = open(out+'.sum.txt', 'w')
         # uniqtab = open(out+'.uniquq.txt', 'w')
-        allagree_path = "".join([out, ".allpair.txt"])
-        allagree = open(out+'.allpair.txt', 'w')
-        # sumtab.write('chr\tpos\tsource\t{}\n'.format(
-        #     '\t'.join(allcomb.keys())))
-        # uniqtab.write('chr\tpos\tsource\t{}\n'.format(
-        #     '\t'.join(allcomb.keys())))
-        allagree.write('chr\tpos\tsegments\tnpair\tpair.list\n')
+
+        # allagree = open(out+'.allpair.txt', 'w')
+        # # sumtab.write('chr\tpos\tsource\t{}\n'.format(
+        # #     '\t'.join(allcomb.keys())))
+        # # uniqtab.write('chr\tpos\tsource\t{}\n'.format(
+        # #     '\t'.join(allcomb.keys())))
+        # allagree.write('chr\tpos\tsegments\tnpair\tpair.list\n')
 
         oldallpair = set([])
+
+
+        #this sets a counter to determine how many times the max_pair_int branch is entered
+        count:int = 0
 
         while sum(list(map(lambda f: endtest[f], endtest.keys()))) < len(endtest):
             pos = min(newpos.values())
             nowf = findkey(pos, newpos)
         #    print('{0} from {1}'.format(str(pos), ' '.join(nowf)))
             for f in nowf:
+                
                 CHR = str(newline[f].split('\t')[0])
                 if newline[f].split('\t')[4] != 'NA':
                     addibd = set(newline[f].split('\t')[4].split(' '))
@@ -379,8 +387,13 @@ def run(args):
             sumrow = list(map(lambda comb: len(
                 allinter(comb, curr_pair)), allcomb.values()))
             uniqrow = get_uniqrow(1, allcomb, curr_pair, combtab)
-            newallpair = all_agree_pair(curr_pair)
-            outpair = []
+            newallpair: list = all_agree_pair(curr_pair)
+            print(len(newallpair))
+            max_pairs_int: int = is_max_pairs_found(max_pairs, len(newallpair))
+            print(max_pairs_int)
+            
+            outpair: list = []
+
             for pp in newallpair:
                 tool = []
                 for f in curr_pair.keys():
@@ -391,21 +404,58 @@ def run(args):
             if len(outpair) == 0:
                 outpair = ['NA']
 
-            allagree.write(
-                f"{str(CHR)}\t{str(pos)}\tNA\t{len(newallpair)}\t{' '.join(outpair)}\n")
+            if max_pairs_int == 1:
+                print(f"The count is {count}")
+                #update the counter
+                count += 1
 
-            oldallpair = set(newallpair)
+
+                #This will return a 0 if the max pairs == the len(newallpair) and a one if max_pairs is greater 
+                after_max_pair: int = after_max_pair_found( 
+                    max_pairs, len(newallpair)) 
+                    
+                if after_max_pair == 0 and count == 1:
+
+                    max_pairs_str: str = previous_row_str
+
+                    #get the start base position which will be used later in the allpair path
+                    start_bp: str = previous_row_bp
+                    print(start_bp)
+                end_bp: str = str(pos)
+
+                if after_max_pair == 1:
+                    # creating an output path that just has the allpair.txt files and
+                    allagree_path = "".join(
+                        [out, ".", start_bp, "-", end_bp, ".allpair.txt"])
+                    print(max_pairs_str)
+                    #Entering into the get_max_pairs function
+                    get_max_pairs(allagree_path, max_pairs_str, variant_id,
+                                  args.car_file, chr_num)
+
+                    break
+            
+            if max_pairs_int == 0:
+                #This if statement is made to reset the counter if the max_pair integer goes from being 1 back to 0
+                
+                #Reseting the counter
+                count = 0 
 
             # sumtab.write('{0}\t{1}\t{2}\t{3}\n'.format(str(CHR), str(
             #     pos), ",".join(nowf), '\t'.join(map(str, sumrow))))
             # uniqtab.write('{0}\t{1}\t{2}\t{3}\n'.format(str(CHR), str(
             #     pos), ",".join(nowf), '\t'.join(map(str, uniqrow))))
+            max_pairs = len(newallpair)
 
+            #keeping track of the previous row so that it can be used if necessary
+            previous_row_str: str = f"{str(CHR)}\t{str(pos)}\tNA\t{len(newallpair)}\t{' '.join(outpair)}\n"
+            
+            #Also keeping track of the base position
+            previous_row_bp:str = str(pos)
+
+            print(previous_row_bp)
         # sumtab.close()
         # uniqtab.close()
-        allagree.close()
-
-        get_max_pairs(allagree_path, out, variant_id, args.car_file, chr_num)
+        # allagree.close()
 
 
 def main():
