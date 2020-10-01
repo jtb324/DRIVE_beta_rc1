8cc8f6434327f8cd7d2482259c6d5e924da59406
jtb324 <james.baker@vanderbilt.edu>
2020-10-01 10:24:22 -0500

    fixed the bug with a regular expression so that only the allele frequencies are being written to the file. Created another file to handle different webdrivers. Also added a requirements.txt file.

--- gnomad_webscrapper/gnomad_scrapper.py
+++ gnomad_webscrapper/gnomad_scrapper.py
@@ -6,27 +6,33 @@ import os
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from webdriver import determine_webdriver
+import sys
+import re
 
 
 class soup_parser():
-    '''This class will take the html site and create a soup'''
+    '''This class will take the html site and create a soup and 
+    then extract hte deired minor allele frequency'''
 
-    def __init__(self, variant_file_path: str, output_name: str):
-        self.url_start = "https://gnomad.broadinstitute.org/variant/"
+    def __init__(self, variant_file_path: str, output_name: str, browser_name: str, wait_time: int):
+        self.url_start: str = "https://gnomad.broadinstitute.org/variant/"
 
-        self.url_end = "?dataset=gnomad_r2_1"
+        self.url_end: str = "?dataset=gnomad_r2_1"
 
-        self.var_list = self.get_var_list(variant_file_path)
+        self.output_file_name: str = output_name
 
-        self.output_file_name = output_name
+        self.browser: str = browser_name.lower()
+
+        self.time: int = wait_time
+
+        self.var_list: list = self.get_var_list(variant_file_path)
 
         self.delete_log()
 
         gnomad_dict: dict = self.get_freq()
 
         self.dict_to_csv(gnomad_dict)
-
-        self.close_browser()
 
     def get_var_list(self, variant_file_path: str) -> list:
         '''This function will pull out the different rs values'''
@@ -39,25 +45,36 @@ class soup_parser():
         elif variant_file_path[-5:] == ".xlsx":
 
             var_df: pd.DataFrame = pd.read_excel(variant_file_path)
-            print(var_df)
+
         # check if it is an excel file
         elif variant_file_path[-4:] == ".txt":
 
             var_df: pd.DataFrame = pd.read_csv(variant_file_path)
 
         var_list: list = var_df["RS Name"].values.tolist()
-        print(len(var_list))
+
         return var_list
 
     @staticmethod
     def delete_log():
+        '''This method removes the geckodriver.log if it is present from
+        a previous run'''
+
         if os.path.isfile('geckodriver.log'):
 
             os.remove('geckodriver.log')
 
     def get_freq(self) -> dict:
+        '''This function gets the minor allele frequencies for variants of interest'''
 
-        browser = webdriver.Firefox()
+        # Creating a dictionary to act as a handler based on what browser is used
+        browser_handling_dict: dict = {
+            "firefox": determine_webdriver(self.browser),
+            "chrome": determine_webdriver(self.browser)
+        }
+
+        # creating the driver object
+        browser = browser_handling_dict[self.browser]
 
         # creating a dictionary to keep track of the allele frequencies
         gnomad_freq_dict: dict = dict()
@@ -71,9 +88,7 @@ class soup_parser():
             browser.get(full_url)
 
             # finding the table element
-            print(browser.title)
-
-            element = WebDriverWait(browser, 10).until(
+            element = WebDriverWait(browser, self.time).until(
                 EC.presence_of_element_located(
                     (By.CLASS_NAME, "Table__BaseTable-sc-7fgtt2-0.PopulationsTable__Table-yt4zj1-0.gRZyOM"))
             )
@@ -81,6 +96,7 @@ class soup_parser():
             frequencies_table = browser.find_element_by_class_name(
                 "Table__BaseTable-sc-7fgtt2-0.PopulationsTable__Table-yt4zj1-0.gRZyOM")
 
+            # Getting the text from the table element
             pop_freq_txt: str = frequencies_table.text
 
             # This section gets the first index
@@ -88,36 +104,50 @@ class soup_parser():
 
             start_indx = start_indx + len("(non-Finnish)")
 
-            # find second index
-            end_indx: int = pop_freq_txt.find("A", start_indx)
+            # Get a substring that starts at the end of the non-Finnish
+            pop_freq_substring: str = pop_freq_txt[start_indx:]
 
-            allele_freq: str = pop_freq_txt[start_indx:end_indx].split(" ")[
-                3].strip("\n")
+            # Use a regular expression to find The first letter in the substring
+            letter_match = re.search("[a-zA-Z]", pop_freq_substring)
+
+            first_letter: str = letter_match.group(0)
+
+            # find second index
+            end_indx: int = pop_freq_substring.find(first_letter)
+
+            allele_freq: str = pop_freq_substring[:end_indx].split(" ")[
+                3].strip("\n").strip(" ")
+
+            print(
+                f"This is the allele frequencies for the variant {variant} is {allele_freq}")
 
             # adding the variant and the allele frequencies to a dictionary
 
             gnomad_freq_dict[variant] = allele_freq
 
         # closign the webdriver
-        self.browser = browser
+        browser.close()
+        browser.quit()
 
         return gnomad_freq_dict
-
-    def close_browser(self):
-        self.browser.quit()
 
     def dict_to_csv(self, dict: dict):
 
         # convert dictionary to dataframe
         gnomad_df = pd.DataFrame(dict.items(), columns=["RS Name", "MAF"])
-        print(gnomad_df)
+
+        # writing the dataframe to a file
         gnomad_df.to_csv(self.output_file_name, index=False, sep="\t")
 
 
 def run(args):
     "function to run"
+    # Checks to make sure that the browser being used is a supported browser
+    if args.browser.lower() not in ["chrome", "firefox"]:
+        print("please use one of the two supported web browser: Firefox or Google Chrome")
+        sys.exit(1)
 
-    soup_parser(args.var_file, args.output)
+    soup_parser(args.var_file, args.output, args.browser, args.wait_time)
 
 
 def main():
@@ -126,8 +156,15 @@ def main():
 
     parser.add_argument("-v", help="This argument provides the file path to a file that list the variants, or a csv/excel file that has a specific column with the RS values",
                         dest="var_file", type=str, required=True)
+
     parser.add_argument("-o", help="This argument provides output path for the file",
                         dest="output", type=str, required=True)
+
+    parser.add_argument("-b", help="This argument provides name of the internet browser used",
+                        dest="browser", type=str, required=True)
+
+    parser.add_argument("-t", help="This parameter gives a amount of time in seconds that the webdriver should wait for a http response. This defaults to 20",
+                        dest="wait_time", default=20, type=int, required=True)
 
     parser.set_defaults(func=run)
     args = parser.parse_args()
