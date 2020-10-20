a3ebe45bcdacf994605e9448d8916ca6cc0a7f40
jtb324 <james.baker@vanderbilt.edu>
2020-10-20 09:38:28 -0500

    Example of the shell script used to run the get_haplotype.py script

---
+++ shell_script/haplotype.sh
@@ -0,0 +1,5 @@
+python3 get_haplotype.py \
+    --ilash /data100t1/share/BioVU/shapeit4/Eur_70k/iLash/min100gmap/ \
+    --hapibd /data100t1/share/BioVU/shapeit4/Eur_70k/hapibd/ \
+    --pairs /data100t1/home/james/MEGA_CLI_project/Testrun/clinical_mega_snps_10_1_20/network_imgs/pairs_in_networks.csv \
+    --output /data100t1/home/james/MEGA_CLI_project/Testrun/clinical_mega_snps_10_1_20/network_imgs/
