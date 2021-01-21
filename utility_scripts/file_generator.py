1e406eb584be2dea42ce58e301a951ab94e8dea5
jtb324 <james.baker@vanderbilt.edu>
2021-01-21 12:42:51 -0600

    work on creating a documentation class that can be used to create readmes in every directory within the project.

--- utility_scripts/file_generator.py
+++ utility_scripts/file_generator.py
@@ -1,14 +1,59 @@
 # This file will contain the parent class for generating readme and log files
 import os
+from os import path
+import getpass
 from datetime import datetime
+
+# This is the base class for creating documentation
 
 
 class Documentation:
-    '''This class will be the parent class for the documentation throughout the program'''
-
+    '''This class will be the parent class for the documentation throughout
+    the program'''
     def __init__(self, file_name: str, output_path: str):
 
         self.file_name = "".join([output_path, file_name])
+        current_time = datetime.now()
+        self.day = current_time.strftime("%b %d, %Y")
+        self.time = current_time.strftime("%H:%M:%S")
+        self.user = getpass.getuser()
 
-    def create_file(self):
-        with open(self.file_name, "w+") as file:
+    def rm_previous_file(self):
+        '''This function will remove the readme from a previous run'''
+
+        if path.exists(self.file_name):
+
+            os.remove(self.file_name)
+
+    def create_date_info(self):
+        '''This function will create a file with a header'''
+
+        with open(self.file_name, "a+") as file:
+
+            # Writing header information to the file
+            file.write(f"run started on {self.day} at {self.time}\n")
+            file.write(f"run started by user: {self.user}\n")
+
+
+class Readme(Documentation):
+    '''This class will extend the Documentation class'''
+    def __init__(self, file_name: str, output_path: str):
+
+        super().__init__(file_name, output_path)
+
+        def write_header(self, header_str: str, directory_name: str):
+            '''This function will write a fairly genetic header for the file'''
+
+            with open(self.file_name, "a+") as readme_file:
+
+                readme_file.write(
+                    f"# README for the {directory_name} directory:\n")
+
+        def add_line(self, info_str: str):
+            '''This function will the info_str to a new line in the document'''
+
+            with open(
+                    self.file_name,
+                    "a+",
+            ) as readme_file:
+                readme_file.write(info_str + "\n")
--- utility_scripts/readme_text/readme_body_text.py
+++ utility_scripts/readme_text/readme_body_text.py
@@ -1,7 +1,20 @@
 # This file contains text that will be used in making the readmes of each file
 
-plink_readme_header: str = "# README for the plink_output_files/ directory:\n"
+# This next section contains text that will be put in the main readme file within the parent directory
+main_parameter_text: str = "* *Information about parameters used during the run can be found in the file mega_run.log within the main directory*\n"
 
-plink_body_text: str = "This directory contains the output from plink while running the initial steps of the program. There should be a ped, map, raw, freq file, and a log file output by plink\n"
+main_directory_header: str = "## Direectory Structure:\n"
 
-carrier_analysis_header: str = "#README for the carrier_analysis_output/ directory:\n"
+main_directory_txt: str = "* **carrier_analysis_output**: This directory contains output that list iids that are identified as carrying a variant of interest on the MEGA_Ex array. These files usually end in single_variant_carrier.csv. This directory also has a file 'allele_frequencies.txt' that describes the allele frequencies for each variant based on Piper's dataset.\n\n* **formated_ibd_output**: This directory contains the output from the steps of the program that convert the ibd_files to a more human readable format. These files are the .small.txt.gz files. There are also output files from the steps that determine which pairs share segments. These files are the allpair.txt files. This directory also contains a file titled confirmed_carriers.txt that list which of the carriers identified on the MEGA array are actual confirmed carriers based on the shared segment analysis.\n\n* **networks**: This directory contains information about the percent of confirmed carriers for each variant (this is found in the carriers_in_networks.csv file) and about which network the confirmed carriers belong to (This is found in the network_groups.csv file within the network_imgs subdirectory).\n\n* **plink_output_files**: This directory contains the output from running the recode, recodeA, and freq option for plink.\n\n* **shell_scripts**:This directory contains the script that was used to run the program"
+
+# The next section contains text that will be inserted into the different
+plink_readme_body_text: str = "* This directory contains the output from plink while running the initial steps of the program. There should be a ped, map, raw, freq file, and a log file output by plink.\nThis files will be split up per chromosome. The main three types of files used during the analysis are the ped, map, and the raw files.\n"
+
+# This next section contains text that will be placed within the readme of the carrier_analysiss_output directory
+carrier_analysis_body_txt: str = "* This directory contains the output that describes which IIDs were detected as carrier specific variants of interest"
+
+# This section contains text for the README that will be placed in the
+# formated_ibd_output directory
+formated_ibd_dir_body_text_1: str = "* This directory contains the output from the mid steps where grids that share segments are identified from the ILASH and hapibd files\n"
+
+formated_ibd_dir_body_text_2: str = "* *More infomation about the location of the iLASH and hapid files cn be found in the mega_run.log file*"
