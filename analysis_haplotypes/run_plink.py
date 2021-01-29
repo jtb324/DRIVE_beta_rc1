12effa61d607316ea269bc0635508d613e02c941
jtb324 <james.baker@vanderbilt.edu>
2021-01-28 20:44:03 -0600

    updated the analyses_haplotype script so that it can get files containing the genotype

--- analysis_haplotypes/run_plink.py
+++ analysis_haplotypes/run_plink.py
@@ -3,22 +3,25 @@ import subprocess
 import os
 
 
-def get_pair_haplotype_str(binary_file: str, var_to_keep_file: str,
-                           start_bp: str, end_bp: str, output_dir_path: str,
-                           ibd_program: str, chr_num: int) -> str:
+def get_plink_haplotype_str(binary_file: str, var_to_keep_file: str,
+                            start_bp: str, end_bp: str, output_dir_path: str,
+                            ibd_program: str, chr_num: str, pair_1: str,
+                            pair_2: str) -> str:
     '''This function will run plink to extract the haplotype of each pair'''
 
     # Creating an output path
-    output_dir: str = "".join([output_dir_path, "haplotype_analysis/"])
-
+    output_dir: str = output_dir_path
     try:
         os.mkdir(output_dir)
 
     except FileExistsError:
         pass
 
-    full_output_path: str = "".join(
-        [output_dir, "plink_", ibd_program, "_", start_bp, "_", end_bp])
+    full_output_path: str = "".join([
+        output_dir, "plink_", ibd_program, "_", pair_1, "_", pair_2, "_",
+        str(start_bp), "_",
+        str(end_bp)
+    ])
 
     subprocess.run(
         [
@@ -27,11 +30,11 @@ def get_pair_haplotype_str(binary_file: str, var_to_keep_file: str,
             binary_file,
             "--keep",
             var_to_keep_file,
-            "from-bp",
+            "--from-bp",
             start_bp,
-            "--to_bp",
+            "--to-bp",
             end_bp,
-            "--chr"
+            "--chr",
             chr_num,
             "--recode",
             "--out",
