04caa38f4ac50bd3481c2a79aa6ebf3fed1493ed
jtb324 <james.baker@vanderbilt.edu>
2021-03-31 11:48:51 -0500

    corrected the imports in the test_shared_segment_formatter.py file

--- test/test_shared_segment_formatter.py
+++ test/test_shared_segment_formatter.py
@@ -4,8 +4,35 @@ import pandas as pd
 import collections
 sys.path.append("../drive")
 
-from pre_shared_segments_analysis_scripts.Shared_segment_Formatter_CLI import find_match, get_file_dict, create_iid_dict, create_dict_with_var_pos, form_chr_num, create_no_carriers_file
+from pre_shared_segments_analysis_scripts.Shared_segment_Formatter_CLI import create_iid_dict, create_dict_with_var_pos, create_no_carriers_file
 
+
+def test_create_iid_dict():
+    """unit test for the create_iid_dict to make sure it is adding the value to the dictionary"""
+
+    # making a log to keep the errors
+    errors: list = []
+
+    variant: str = "exm65234"
+
+    iid_dict: dict = {}
+
+    carrier_dict: dict = {
+        "IID": ["R123541334", "R098764567", "r1234568"],
+        "Variant ID":["exm65234", "exm65234", "exm65234"]
+    }
+
+    carrier_df: pd.DataFrame = pd.DataFrame.from_dict(carrier_dict)
+
+    iid_dict = create_iid_dict(variant, iid_dict, carrier_df)
+
+    if len(iid_dict[variant]) != 3:
+        errors.append(f"expected the length of the value for the key {variant} to be 3, instead the length was {len(iid_dict[variant])}")
+    
+    if variant not in iid_dict.keys():
+        errors.append(f"Variant, {variant}, was expected to be in the key list of the iid_dict. The value was not found")
+    
+    assert not errors, "errors occured: \n{}".format('\n'.join(errors))
 
 def test_create_dict_with_var_pos():
     """unit test for the create_dict_with_var_pos function to make sure it is also adding the value correctly"""
