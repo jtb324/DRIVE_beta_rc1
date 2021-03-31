6a881ae74dc9f9171ef200cd5ada61c9ce8a6e7c
jtb324 <james.baker@vanderbilt.edu>
2021-03-31 11:42:25 -0500

    added a description sentence to the function

--- test/test_argparse_args.py
+++ test/test_argparse_args.py
@@ -8,6 +8,8 @@ def run():
     pass
 
 def test_create_args():
+    """unit test to make sure the parser from the create_args function is returning the correct type of argument"""
+    
     parser = parser_arguments.create_args(run)
 
     assert type(parser) == argparse.ArgumentParser, "the parser is not the correct type"
