6e83523a5b64785dd00e495081d03024a8629849
jtb324 <james.baker@vanderbilt.edu>
2021-05-14 11:48:25 -0500

    updated the unittest so that they will work

--- test/test_initial_parameters.py
+++ test/test_initial_parameters.py
@@ -1,47 +1,33 @@
 # Setting the system path
 import logging
 import sys
+from typing import List
 sys.path.append('../drive')
 
-from utility_scripts.user_input.initial_parameters import Input_Gather, get_dict_of_variables
+from utility_scripts.user_input.initial_parameters import  check_provided_maf, check_provided_integer
 # Importing modules
 
-input_gather_object = Input_Gather()
+def test_check_provided_maf():
+    """unit test to check if the returned maf is correct"""
+    error_list: List[str] = []
+    maf_threshold: float = check_provided_maf(0.10)
 
+    if type(maf_threshold) != float:
+        error_list.append(f"The resulting output is not of the expected type float instead it is: {(type(maf_threshold))}")
+    if maf_threshold != 0.10:
+        error_list.append(f"Expected the output to be 0.10 instead it was {maf_threshold}")
 
-def test_class_type():
-    """Function to make suerthe class is of the expected type"""
+    assert not error_list, "errors occured: \n{}".format('\n'.join(error_list))
 
-    assert type(input_gather_object) == Input_Gather
+def test_check_provided_integer():
+    """unit test to check if the returned maf is correct"""
+    error_list: List[str] = []
+    return_integer: int = check_provided_integer(3)
 
+    if type(return_integer) != int:
+        error_list.append(f"The resulting output is not of the expected type float instead it is: {(type(return_integer))}")
 
-def test_analysis_type():
-    """Function to make sure that the analysis type will be caught if it is not a recognized argument"""
+    if return_integer != 3:
+        error_list.append(f"Expected the output to be 0.10 instead it was {return_integer}")
 
-    assert input_gather_object.ANALYSIS_TYPE in ["gene", "maf", ""]
-
-
-def test_min_cm():
-    """This value test to make sure that the minimum centimorgan value is a integer"""
-
-    assert type(input_gather_object.MIN_CM) == int
-
-
-def test_threads():
-    """Function checks to make sure that the number of threads passed to the program is an integer"""
-
-    assert type(input_gather_object.THREADS) == int
-
-
-def test_maf_filter():
-    """unit test to make sure that the minor allele frequency threshold is a string"""
-    assert type(input_gather_object.MAF_THRESHOLD) == str
-
-
-def test_dictionary_keys():
-    """unit test to make sure that all the expected dictionary keys are in the returned dictionary"""
-    parameters_dict: dict = get_dict_of_variables(input_gather_object)
-
-    para_keys: list = list(parameters_dict.keys())
-
-    assert "ANALYSIS_TYPE" in para_keys and "MIN_CM" in para_keys and "THREADS" in para_keys and "MAF_THRESHOLD" in para_keys
+    assert not error_list, "errors occured: \n{}".format('\n'.join(error_list))
